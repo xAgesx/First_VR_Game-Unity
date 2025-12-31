6ddbc1513f9c6aedb38c9c5fa3186c9dc3687e8b
xAgesx <thamer.douss@gmail.com>
2025-12-31 23:49:10 +0100

    added brewing effect

--- Assets/Scripts/ovenCollider.cs
+++ Assets/Scripts/ovenCollider.cs
@@ -6,6 +6,7 @@ public class ovenCollider : MonoBehaviour {
     [SerializeField] Oven ovenScript;
     [SerializeField] Material transformedMat;
     [SerializeField] GameObject progressCanvasPrefab; 
+    
     [SerializeField] Vector3 offset = new Vector3(0, 0.5f, 0); 
     [SerializeField] float transformationDuration = 3.0f;
 
@@ -34,63 +35,5 @@ public class ovenCollider : MonoBehaviour {
                 tracker.StopTransforming();
             }
         }
-    }
-}
-
-public class PotionTracker : MonoBehaviour {
-    public bool isTransformed = false;
-    public bool isCooking = false;
-    
-    private Coroutine activeRoutine;
-    private GameObject activeUI;
-
-    public void StartTransforming(float duration, Material newMat, GameObject uiPrefab, Vector3 offset) {
-        isCooking = true;
-        activeRoutine = StartCoroutine(TransformRoutine(duration, newMat, uiPrefab, offset));
-    }
-
-    public void StopTransforming() {
-        if (isTransformed) return; // Don't stop if it's already finished
-
-        isCooking = false;
-        if (activeRoutine != null) {
-            StopCoroutine(activeRoutine);
-            activeRoutine = null;
-        }
-
-        if (activeUI != null) {
-            Destroy(activeUI);
-        }
-    }
-
-    private IEnumerator TransformRoutine(float duration, Material newMat, GameObject uiPrefab, Vector3 offset) {
-        Slider progressBar = null;
-
-        // Create UI
-        if (uiPrefab != null) {
-            activeUI = Instantiate(uiPrefab, transform.position + offset, Quaternion.identity, transform);
-            progressBar = activeUI.GetComponentInChildren<Slider>();
-        }
-
-        float elapsed = 0;
-        while (elapsed < duration) {
-            elapsed += Time.deltaTime;
-            if (progressBar != null) {
-                progressBar.value = elapsed / duration;
-            }
-            yield return null;
-        }
-
-        // Complete Transformation
-        MeshRenderer renderer = GetComponent<MeshRenderer>();
-        if (renderer != null && newMat != null) {
-            Material[] mats = renderer.sharedMaterials;
-            for (int i = 0; i < mats.Length; i++) mats[i] = newMat;
-            renderer.materials = mats;
-        }
-
-        isTransformed = true;
-        isCooking = false;
-        if (activeUI != null) Destroy(activeUI);
     }
 }
