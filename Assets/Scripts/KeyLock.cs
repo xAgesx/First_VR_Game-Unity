30bd24bfbb2896175434664ee1487a95bb6e6b53
xAgesx <thamer.douss@gmail.com>
2026-01-01 22:34:53 +0100

    added OpenDoor cutscene

--- Assets/Scripts/KeyLock.cs
+++ Assets/Scripts/KeyLock.cs
@@ -1,11 +1,16 @@
 using UnityEngine;
+using UnityEngine.Playables;
 
-public class KeyLock : MonoBehaviour
-{   
+public class KeyLock : MonoBehaviour {
     [SerializeField] GameObject helpUI;
+    [SerializeField] PlayableDirector unlockTimeline;
+
     void OnTriggerEnter(Collider other) {
         if (other.CompareTag("Rusty_Key")) {
             helpUI.SetActive(true);
+        } else if (other.CompareTag("Key")) {
+            unlockTimeline.Play();
+            Destroy(other.gameObject);
         }
     }
 }
